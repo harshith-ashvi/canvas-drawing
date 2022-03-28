f14f47700b68384508de236df31677d22f70affb
harshith <harshith.ashvi@gmail.com>
2022-03-28 14:45:03 +0530

    add resizing options to the added elements

--- src/components/canvasDrawing/CanvasDrawing.js
+++ src/components/canvasDrawing/CanvasDrawing.js
@@ -23,25 +23,84 @@ const createElement = (id, x1, y1, x2, y2, type) => {
 
 const distance = (a, b) => Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2))
 
-const isWithinElement = (x, y, element) => {
+const nearPoint = (x, y, x1, y1, name) => {
+    return Math.abs(x - x1) < 5 && Math.abs(y - y1) < 5? name:null
+}
+
+const positionWithinElement = (x, y, element) => {
     const { type, x1, x2, y1, y2 } = element
     if (type === "rectangle"){
-        const minX = Math.min(x1, x2)
-        const maxX = Math.max(x1, x2)
-        const minY = Math.min(y1, y2)
-        const maxY = Math.max(y1, y2)
-        return x >= minX && x <= maxX && y >= minY && y <= maxY
+        const topLeft = nearPoint(x, y, x1, y1, "tl")
+        const topRight = nearPoint(x, y, x2, y1, "tr")
+        const bottomLeft = nearPoint(x, y, x1, y2, "bl")
+        const bottomRight = nearPoint(x, y, x2, y2, "br")
+        const inside = x >= x1 && x <= x2 && y >= y1 && y <= y2 ? 'inside':null
+        return topLeft || topRight || bottomLeft || bottomRight || inside
     } else {
         const a = { x: x1, y: y1 }
         const b = { x: x2, y: y2 }
         const c = { x, y }
         const offset = distance(a, b) - (distance(a, c) + distance(b, c))
-        return Math.abs(offset) < 1
+        const start = nearPoint(x, y, x1, y1, "start")
+        const end = nearPoint(x, y, x2, y2, "end")
+        const inside = Math.abs(offset) < 1? "inside":null
+        return start || end || inside
     }
 }
 
 const getElementAtPosition = (x, y, elements) => {
-    return elements.find(element => isWithinElement(x, y, element))
+    return elements
+        .map(element => ({...element, position: positionWithinElement(x, y, element)}))
+        .find(ele => ele.position !== null)
+}
+
+const adjustElementsCoordinates = ({ x1, y1, x2, y2, type }) => {
+    if(type === "rectangle"){
+        const minX = Math.min(x1, x2)
+        const maxX = Math.max(x1, x2)
+        const minY = Math.min(y1, y2)
+        const maxY = Math.max(y1, y2)
+        return { x1: minX, y1: minY, x2: maxX, y2: maxY }
+    } else {
+        if(x1 < x2 || (x1 === x2 && y1 < y2)){
+            return { x1, y1, x2, y2 }
+        } else {
+            return { x1: x2, y1: y2, x2: x1, y2: y1 }
+        }
+    }
+}
+
+const cursorForPosition = (position) => {
+    switch (position) {
+        case "tl":
+        case "br":
+        case "start":
+        case "end":
+            return "nwse-resize"
+        case "tr":
+        case "bl":
+            return "nesw-resize"
+        default: 
+            return "move"
+    }
+}
+
+const resizedCoordinates = (clientX, clientY, position, coordinates) => {
+    const { x1, y1, x2, y2 } = coordinates
+    switch (position) {
+        case "tl": 
+        case "start":
+            return { x1: clientX, y1: clientY, x2, y2 } 
+        case "tr": 
+            return { x1, y1: clientY, x2: clientX, y2 }
+        case "bl":
+            return { x1: clientX, y1, x2, y2: clientY }
+        case "br":
+        case "end":
+            return { x1, y1, x2: clientX, y2: clientY }
+        default : 
+            return null
+    }
 }
 
 const CanvasDrawing = () => {
@@ -81,12 +140,18 @@ const CanvasDrawing = () => {
                 const offsetX = clientX - element.x1
                 const offsetY = clientY - element.y1
                 setSelectedElement({...element, offsetX, offsetY})
-                setAction("moving")
+                if(element.position === "inside"){
+                    setAction("moving")
+                } else {
+                    setAction("resizing")
+                }
+                
             }
         } else {
             const id = elements.length
             const element = createElement(id, clientX, clientY, clientX, clientY)
             setElements(prevState => [...prevState, element])
+            setSelectedElement(element)
             setAction("drawing")
         }
         
@@ -94,9 +159,9 @@ const CanvasDrawing = () => {
 
     const handleMouseMove = (event) => {
         const { clientX, clientY } = event
-
+        const element = getElementAtPosition(clientX, clientY, elements)
         if(tool === "selection"){
-            event.target.style.cursor = getElementAtPosition(clientX, clientY, elements)? "move":"default"
+            event.target.style.cursor = element? cursorForPosition(element.position):"default"
         }
 
         if (action === 'drawing') {
@@ -110,8 +175,11 @@ const CanvasDrawing = () => {
             const height = y2 - y1
             const newX1 = clientX - offsetX
             const newY1 = clientY - offsetY
-            updateElement(id, newX1, newY1, newX1 + width, newY1 + height, type)
-        
+            updateElement(id, newX1, newY1, newX1 + width, newY1 + height, type)  
+        } else if (action === 'resizing') {
+            const { id, type, position, ...coordinates } = selectedElement
+            const { x1, y1, x2, y2 } = resizedCoordinates(clientX, clientY, position, coordinates)
+            updateElement(id, x1, y1, x2, y2, type)
         }
 
         
@@ -119,6 +187,13 @@ const CanvasDrawing = () => {
     }
 
     const handleMouseUp = () => {
+        const index = selectedElement.id
+        const {id, type} = elements[index]
+        if (action === "drawing" || action === "resizing"){
+            const { x1, y1, x2, y2 } = adjustElementsCoordinates(elements[index])
+            updateElement(id, x1, y1, x2, y2, type)
+        }
+        
         setAction("none")
         setSelectedElement(null)
     }
